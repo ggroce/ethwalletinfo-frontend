92a32dcbd38ca2e967771ff1a840ddf6e75a105c
ggroce <ggrocedev@gmail.com>
2022-05-22 21:33:15 -0500

    changes to prevent NFTs without relevant metadata from appearing in card list

--- src/components/NftCard/NftCard.jsx
+++ src/components/NftCard/NftCard.jsx
@@ -4,10 +4,14 @@ import { useState } from 'react';
 
 const NftCard = (props) => {
   const [showDetails, setShowDetails] = useState(false);
+  if (!props?.meta?.content || props?.meta?.content?.length === 0) {
+    return null;
+  }
+  let imageUrls = null;
 
-  const imageUrls = props.meta.content.reduce((prev, curr, index) => {
+  imageUrls = props?.meta?.content?.reduce((prev, curr, index) => {
     return { ...prev, 
-      [curr.representation]: curr.url,
+      [curr?.representation]: curr?.url,
     }
   }, {});
 
@@ -15,14 +19,14 @@ const NftCard = (props) => {
     <>
       <div className="nftcard__wrapper" onClick={() => setShowDetails(true)}>
         <div className="nftcard__info-wrapper">
-          <h5>{props.meta?.name}</h5>
+          <h5>{props?.meta?.name || 'Name N/A'}</h5>
         </div>
         <img
           className="nftcard__img"
-          src={imageUrls.PREVIEW || imageUrls.ORIGINAL}
+          src={imageUrls?.PREVIEW || imageUrls?.ORIGINAL}
           loading="lazy"
           title="Picture of NFT"
-          alt={props.meta?.name}
+          alt={props?.meta?.name || 'Name N/A'}
         />
       </div>
       {showDetails && (
