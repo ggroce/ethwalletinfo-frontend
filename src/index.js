9d9caa99738a861d02fea63456531d71c0acee1a
ggroce <ggrocedev@gmail.com>
2022-06-03 18:23:26 -0500

    updated dependencies

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 import './index.css';
 import App from './containers/App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
